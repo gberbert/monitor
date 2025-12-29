c48886e7ceed83134b849340eae9ad856bc50eae
Gustavo Costa Berbert <gustavo.berbert@gmail.com>
2025-12-29 16:57:20 -0300

    versão 1.0.18

--- www/version.js
+++ www/version.js
@@ -1,4 +1,4 @@
-const appVersion = "1.0.17"; 
+const appVersion = "1.0.18"; 
 document.addEventListener("DOMContentLoaded", () => {
     const versionEl = document.getElementById('app-version-display');
     if (versionEl) {
